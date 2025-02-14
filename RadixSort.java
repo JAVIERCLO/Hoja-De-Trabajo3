f6482bb4e2f883c48f2fd555087b857dafe269b0
Pjezz <p.jezraelhuerta@gmail.com>
2025-02-13 22:57:09 -0600

    Terminando la clase RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -1,5 +1,63 @@
-public class RadixSort<T extends Comparable<T>> implements ISort<t> {
+import java.util.Arrays;
+
+public class RadixSort<T extends Comparable<T>> implements ISort<T> {
     @Override
     public void sort(T[] array) {
-        
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        int[] intArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            intArray[i] = array[i].hashCode();
+
+        }
+        radixSort(intArray);
+
+        for (int i = 0; 1 < array.length; i++) {
+            array[i] = (T) Integer.valueOf(intArray[i]);
+
+        }
+    }
+
+    private int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    private void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+
+    }
+
+    private void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
 }
