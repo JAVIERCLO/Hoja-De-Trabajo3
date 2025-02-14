7125cea1ac194b736f0af2e965120074b77168f2
Pjezz <p.jezraelhuerta@gmail.com>
2025-02-13 22:44:45 -0600

    Borrando error en InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -5,17 +5,17 @@ public class InsertionSort<T extends Comparable<T>> implements ISort<T> {
         if (array == null || array.length == 0) {
             return;
         }
-    
+
         int n = array.length;
-        for (int i = 1; i < n; ++i ){
-            T key = array.[1];
+        for (int i = 1; i < n; ++i) {
+            T key = array[1];
             int j = i - 1;
 
             while (j >= 0 && array[j].compareTo(key) > 0) {
                 array[j + 1] = array[j];
                 j = j - 1;
+            }
+            array[j + 1] = key;
         }
-        array[j + 1] = key;
-      }
     }
 }
--- RadixSort.java
+++ RadixSort.java
@@ -1,1 +1,5 @@
-public
+public class RadixSort<T extends Comparable<T>> implements ISort<t> {
+    @Override
+    public void sort(T[] array) {
+        
+}
